9e1fcf19b7faa7486e6aaded3ad515760e2c5526
LukacTomas <lukactv@gmail.com>
2021-10-14 08:51:44 +0200

    get_lastline_number_from_file refactored to header file, makefile changed accordingly

---
+++ fileread.h
@@ -0,0 +1,4 @@
+#ifndef FILEREAD_H_
+#define FILEREAD_H_
+unsigned int get_lastline_number_from_file(char *filename);
+#endif
--- main.c
+++ main.c
@@ -2,45 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-
-unsigned int get_lastline_number_from_file(char *filename)
-{
-   
-    FILE *file = fopen(filename, "r");
-    if (!file)
-    {
-        perror("Error");
-        exit(-1);
-    }
-    printf("File <%s> was succesfully open for reading\n", filename);
-    printf("Getting number from last line\n");
-
-    // MAX_LEN of last line to be read to buffer
-    // e.g max two 10-digit int-s with space and \n 
-    // (2*10 + 1 + 1) = 22
-    static const long MAX_LEN = 22;
-    char buffer[MAX_LEN + 1];
-
-    // Read Last Line from File
-    int setLastLine = fseek(file, -MAX_LEN, SEEK_END); 
-    int readLastLineToBuffer = fread(buffer, MAX_LEN - 1, 1, file);
-    int closeFile = fclose(file);
-    buffer[MAX_LEN - 1] = 0;
-    char *lastLine = strrchr(buffer, '\n') + 1;
-
-    // Get First String (number) From Last Line
-    int i = 0;
-    while (lastLine[i] != ' ')
-    {
-        buffer[i - 1] = lastLine[i++];
-    }
-    buffer[i] = 0;
- 
-    int number = atoi(buffer);
-    printf("Last line number is : %d\n", number);
-
-    return number;
-}
+#include "fileread.h"
 
 int main(int argc, char **argv)
 {
